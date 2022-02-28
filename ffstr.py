51cbf68ef213a4e4cc67e760604f063cd5056b19
Electro-Bug <3l3ctr0bug@gmail.com>
2022-02-28 20:49:44 +0100

    help function added

--- ffstr.py
+++ ffstr.py
@@ -519,9 +519,32 @@ class ffstr():
 			# Close connexion
 			if not all(self.blind_behavior):
 				self.close()
-				
-if __name__ == "__main__":
 
+def help():
+	print(
+	"""
+	!!! WARNING BETA VERSION !!!
+	
+	Usage python3 ffstr.py options
+	
+	Options:
+	
+	BINARY          Link to the chall either as a path ./chall or url 127.0.0.1:1337
+	ELF             Link to the supporting elf file, path only
+	BITS            32 or 64 bits
+	
+	Examples:
+	
+	python3 ffstr.py BINARY=127.0.0.1:1337 BITS=64 ELF=./ffstr64
+	python3 ffstr.py BINARY=127.0.0.1:1337 BITS=32 ELF=./ffstr32
+	python3 ffstr.py BINARY=./ffstr64 BITS=64 ELF=./ffstr64
+	python3 ffstr.py BINARY=./ffstr32 BITS=32 ELF=./ffstr32
+	""")
+if __name__ == "__main__":
+	
+	if not args:
+		help()
+		exit()
 	# ffstr instanciation
 	exploit = ffstr()
 	exploit.getArgs()
